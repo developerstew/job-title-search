a7e8548044748dc2ea781632696d91f86658460e
developerstew <dewald.greeff@hyvemobile.co.za>
2024-08-25 16:22:46 +0200

    experiment with different client

--- src/app/utils/trpc/client.ts
+++ src/app/utils/trpc/client.ts
@@ -15,7 +15,7 @@ const getBaseUrl = () => {
     return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
 };
 
-export const trpc = createTRPCNext<AppRouter>({
+export const client = createTRPCNext<AppRouter>({
     ssr: true,
     ssrPrepass,
     config() {
@@ -26,6 +26,16 @@ export const trpc = createTRPCNext<AppRouter>({
                     transformer: superjson,
                     url: `${getBaseUrl()}/api/trpc`,
                 }),
+                // headers() {
+                //     if (!ctx?.req?.headers) {
+                //       return {};
+                //     }
+                //     // To use SSR properly, you need to forward client headers to the server
+                //     // This is so you can pass through things like cookies when we're server-side rendering
+                //     return {
+                //       cookie: ctx.req.headers.cookie,
+                //     };
+                //   },
             ],
         };
     },
